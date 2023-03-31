3a52a4898c8185f19fa1d30cdccdc3104d086bb3
Rosamaria4512 <rmquilindo8@misena.edu.co>
2023-03-30 20:25:05 -0500

    app_recycle

---
+++ app/src/main/java/com/rosa/appreciclaje/MenuReciclaje.java
@@ -0,0 +1,14 @@
+package com.rosa.appreciclaje;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuReciclaje extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_reciclaje);
+    }
+}
---
+++ app/src/main/java/com/rosa/appreciclaje/ReciclaYa.java
@@ -0,0 +1,16 @@
+package com.rosa.appreciclaje;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReciclaYa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reciclaya);
+    }
+
+
+}
