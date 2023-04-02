45ae4a7aaf9c22f330ebf2c649d692dd0a6e50a9
Rosamaria4512 <rmquilindo8@misena.edu.co>
2023-04-02 16:05:20 -0500

    ...

--- app/src/main/java/com/rosa/appreciclaje/MainActivity.java
+++ app/src/main/java/com/rosa/appreciclaje/MainActivity.java
@@ -1,16 +1,58 @@
 package com.rosa.appreciclaje;
 
+import static com.rosa.appreciclaje.R.id.imageViewbtn1;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+       // ImageView boton1 = (ImageView) findViewById(R.id.img_recicla);
+       // boton1.setOnClickListener( (v)->{
+          //  Toast notificacion = Toast.makeText(MainActivity.this, "recicla",Toast.LENGTH_SHORT);
+             //       notificacion.show();
+          //  Intent intencion = new Intent(getApplicationContext(),MenuReciclaya2Activity.class);
+         //   startActivity(intencion);
+      //  });
+
+      //  Button boton1 = (Button) findViewById(R.id.btn_otro);
+       // boton1.setOnClickListener((v)->{
+         //   Toast notificacion = Toast.makeText(MainActivity.this, "recicla",Toast.LENGTH_SHORT);
+          //  notificacion.show();
+          //  Intent intencion = new Intent(getApplicationContext(),MenuReciclaya2Activity.class);
+           //  startActivity(intencion);
+
+      //  });
+
+        ImageView boton2 = (ImageView) findViewById(R.id.imageViewbtn1);
+        boton2.setOnClickListener((v)->{
+            Toast notificacion1= Toast.makeText(MainActivity.this, "Plastico",Toast.LENGTH_SHORT);
+            notificacion1.show();
+            Intent intencion1 = new Intent(getApplicationContext(),ReciclaYa.class);
+            startActivity(intencion1);
+
+        });
+
+        ImageView boton3 = (ImageView) findViewById(R.id.imageViewbtn2);
+
     }
 
+    @Override
+    public void onClick(View view) {
 
+    }
 }
--- app/src/main/java/com/rosa/appreciclaje/MenuReciclaje.java
+++ app/src/main/java/com/rosa/appreciclaje/MenuReciclaje.java
@@ -1,8 +1,15 @@
 package com.rosa.appreciclaje;
 
+import static android.view.MenuInflater.*;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MenuReciclaje extends AppCompatActivity {
 
@@ -10,5 +17,27 @@ public class MenuReciclaje extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_reciclaje);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_reciclaya, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.item_1) {
+            Toast.makeText(this, "metodo1", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.item_2) {
+            Toast.makeText(this, "metodo2", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.item_3) {
+            Toast.makeText(this, "metodo3", Toast.LENGTH_SHORT).show();
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
